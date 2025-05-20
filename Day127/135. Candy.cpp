6a3f361eb83e76edfab43cd164f40cf67a25c213
princee09 <141345546+princee09@users.noreply.github.com>
2025-05-20 13:32:16 +0530

    Add files via upload

---
+++ Day127/135. Candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        if (n <= 1) return n;
+
+        vector<int> candies(n, 1);
+
+        // Left to right pass
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        // Right to left pass
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i+1]) {
+                candies[i] = max(candies[i], candies[i+1] + 1);
+            }
+        }
+
+        // Sum up
+        int total = 0;
+        for (int c : candies) total += c;
+        return total;
+    }
+};
