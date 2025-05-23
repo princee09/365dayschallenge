9f6a079942ea9b0eaa4d52589c33f1bafac3220d
princee09 <141345546+princee09@users.noreply.github.com>
2025-05-23 10:13:58 +0530

    Add files via upload

---
+++ Day130/145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+
+            // Push left first so right is processed first
+            if (node->left) st.push(node->left);
+            if (node->right) st.push(node->right);
+        }
+
+        reverse(result.begin(), result.end()); // Convert root-right-left to left-right-root (postorder)
+        return result;
+    }
+};
