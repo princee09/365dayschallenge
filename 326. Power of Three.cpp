a58526c73eb16561ee5bf57f1ab3ca10bd971c10
princee09 <141345546+princee09@users.noreply.github.com>
2025-08-13 11:03:23 +0530

    Delete 326. Power of Three.cpp

--- 326. Power of Three.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfThree(int n) {
-        // Largest power of 3 in 32-bit signed int: 3^19 = 1162261467
-        return n > 0 && 1162261467 % n == 0;
-    }
-};
