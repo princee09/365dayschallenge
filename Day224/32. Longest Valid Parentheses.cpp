d30ce9a3b1ef3b5d4d018d829be926c03a1caa25
princee09 <141345546+princee09@users.noreply.github.com>
2025-08-25 12:56:35 +0530

    Add files via upload

---
+++ Day224/32. Longest Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        if (n <= 1)
+            return 0;
+
+        stack<int> st;
+        st.push(-1);
+        int maxLen = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+        return maxLen;
+    }
+};
