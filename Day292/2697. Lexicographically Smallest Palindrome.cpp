be6e0210f4925d23358963716cccd67ec9561db0
princee09 <141345546+princee09@users.noreply.github.com>
2025-11-01 15:35:11 +0530

    Add files via upload

---
+++ Day292/2697. Lexicographically Smallest Palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (s[left] != s[right]) {
+                if (s[left] < s[right]) {
+                    s[right] = s[left];
+                } else {
+                    s[left] = s[right];
+                }
+            }
+            left++;
+            right--;
+        }
+        
+        return s;
+    }
+};
