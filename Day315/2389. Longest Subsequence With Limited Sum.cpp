de22e3f4ef1bece3cb6e862c215bc457fd8b424c
princee09 <141345546+princee09@users.noreply.github.com>
2025-11-24 22:20:59 +0530

    Add files via upload

---
+++ Day315/2389. Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        
+        vector<int> ans;
+        for (int q : queries) {
+            int idx = upper_bound(nums.begin(), nums.end(), q) - nums.begin();
+            ans.push_back(idx);
+        }
+        
+        return ans;
+    }
+};
