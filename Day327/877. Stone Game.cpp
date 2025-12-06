9f253dcb9f9f4f96cab908a7eca86e90c72f1ac5
princee09 <141345546+princee09@users.noreply.github.com>
2025-12-06 15:08:19 +0530

    Add files via upload

---
+++ Day327/877. Stone Game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                int pickLeft = piles[i] - dp[i + 1][j];
+                int pickRight = piles[j] - dp[i][j - 1];
+                dp[i][j] = max(pickLeft, pickRight);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+};
