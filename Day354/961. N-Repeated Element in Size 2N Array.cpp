0ccebc8944e2a6cec1157c6afca704270e375b43
princee09 <141345546+princee09@users.noreply.github.com>
2026-01-02 17:00:59 +0530

    Add files via upload

---
+++ Day354/961. N-Repeated Element in Size 2N Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int x;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+            if(i+2<n && nums[i]==nums[i+2]){
+                return nums[i];
+            }
+            if(i+3<n && nums[i]==nums[i+3]){
+                return nums[i];
+            }
+            
+        } 
+        return 0;
+    }
+};
